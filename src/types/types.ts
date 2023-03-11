f5c72478d6d630cd7fdd62de0417e51ef672fec2
Sebastian Mazur <102309928+Blejn@users.noreply.github.com>
2023-03-11 09:45:29 +0100

    added and edit types

--- src/types/types.ts
+++ src/types/types.ts
@@ -3,20 +3,65 @@ export type MyPropsTodo = {
   userName: string;
   todoItems: Todo[];
   newTaskAction: string;
+  modalHandler(todo: Todo): any;
   newTaskDescription: string;
   callback(todo: Todo): any;
+  editTask(actionCurrent: string, action: string, description: string): any;
   itemsDone: boolean;
+  formValid: boolean;
+  deleteTask(array: Array<Todo>, action: string, description: string): any;
 };
 
 export type MyState = {
   userName: string;
   todoItems: Todo[];
   newTaskAction: string;
+
   newTaskDescription: string;
   itemsDone: boolean;
+  formValid: boolean;
+  searchBar: string;
 };
 export interface Todo {
   action: string;
   done: boolean;
   description: string;
+  isModal: boolean;
 }
+
+export interface TodoEdit {
+  action: string;
+  done: boolean;
+  description: string;
+  isModal: boolean;
+}
+export type FormError = {
+  action: string;
+  description: string;
+};
+export type EditStateActual = {
+  item: any;
+  action: string;
+  description: string;
+  isModal: boolean;
+  editTaskAction: string;
+  editTaskDescription: string;
+  modalHandler(todo: Todo): any;
+};
+export type EditProps = {
+  item: any;
+  action: string;
+  description: string;
+  isModal: boolean;
+  editTask(actionCurrent: string, action: string, description: string): any;
+
+  modalHandler(todo: Todo): any;
+};
+export type MyStateCard = {
+  userName: string;
+  todoItems: Todo[];
+  newTaskAction: string;
+  newTaskDescription: string;
+  itemsDone: boolean;
+  formValid: boolean;
+};
