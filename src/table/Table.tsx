27e9e9a8ff455c42593f54929b781567f3828fb5
Sebastian Mazur <102309928+Blejn@users.noreply.github.com>
2023-03-11 09:44:25 +0100

    delete Tasks

--- src/table/Table.tsx
+++ src/table/Table.tsx
@@ -1,14 +1,18 @@
 import React, { Component } from "react";
 import { BsTrash } from "react-icons/bs";
-import { MyPropsTodo, MyState, Todo } from "../types/types";
+import EditTask from "../Edit/EditTask";
+import { MyPropsTodo, MyState, MyStateCard, Todo } from "../types/types";
 
-export default class Table extends Component<MyPropsTodo, MyState> {
+export default class Table extends Component<MyPropsTodo, MyStateCard> {
   constructor(props: MyPropsTodo) {
     super(props);
     this.state = {
       ...this.props,
     };
   }
+  deleteTask = (item: Todo) => {
+    this.props.deleteTask(this.props.todoItems, item.action, item.description);
+  };
 
   render() {
     return (
@@ -19,8 +23,15 @@ export default class Table extends Component<MyPropsTodo, MyState> {
             {this.props.todoItems
               .filter(item => item.done === false)
               .map(item => (
-                <>
-                  <div className="block w-72 h-64 rounded-lg bg-white p-6 shadow-lg dark:bg-[#c471c7]">
+                <div key={item.action}>
+                  <div className="relative block w-72 h-64 rounded-lg bg-white p-6 shadow-lg dark:bg-[#c471c7]">
+                    <button
+                      onClick={() => this.props.modalHandler(item)}
+                      className="absolute  top-2 right-4"
+                    >
+                      Edit
+                    </button>
+
                     {item.done ? (
                       <h5 className="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50  line-through ">
                         {item.action}
@@ -45,6 +56,7 @@ export default class Table extends Component<MyPropsTodo, MyState> {
                             className="inline-block rounded bg-primary px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
                             data-te-ripple-init
                             data-te-ripple-color="light"
+                            onClick={() => this.deleteTask(item)}
                           >
                             Delete
                           </button>
@@ -61,7 +73,22 @@ export default class Table extends Component<MyPropsTodo, MyState> {
                       )}
                     </div>
                   </div>
-                </>
+                  {item.isModal ? (
+                    <>
+                      {" "}
+                      <EditTask
+                        item={item}
+                        action={item.action}
+                        description={item.description}
+                        isModal={item.isModal}
+                        modalHandler={this.props.modalHandler}
+                        editTask={this.props.editTask}
+                      />
+                    </>
+                  ) : (
+                    <></>
+                  )}
+                </div>
               ))}
           </>
         ) : (
@@ -69,7 +96,7 @@ export default class Table extends Component<MyPropsTodo, MyState> {
             {this.props.todoItems
               .filter(item => item.done === true)
               .map(item => (
-                <>
+                <div key={item.action}>
                   <div className="block w-72 h-64 rounded-lg bg-white p-6 shadow-lg dark:bg-[#c471c7]">
                     {item.done ? (
                       <h5 className="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50  line-through ">
@@ -95,6 +122,13 @@ export default class Table extends Component<MyPropsTodo, MyState> {
                             className="inline-block rounded bg-primary px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
                             data-te-ripple-init
                             data-te-ripple-color="light"
+                            onClick={() =>
+                              this.props.deleteTask(
+                                this.props.todoItems,
+                                item.action,
+                                item.description
+                              )
+                            }
                           >
                             Delete
                           </button>
@@ -111,7 +145,7 @@ export default class Table extends Component<MyPropsTodo, MyState> {
                       )}
                     </div>
                   </div>
-                </>
+                </div>
               ))}
           </>
         )}
